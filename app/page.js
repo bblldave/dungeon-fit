d1b644a72b8ed341b6bc629c219d3ec0b72c3943
bblldave <opticalmousepilot@gmail.com>
2024-03-16 14:07:45 -0400

    feature: Setup main layout

--- app/page.js
+++ app/page.js
@@ -5,8 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button type="primary">Primary Button</Button>
-      <Button type="dashed">Primary Button</Button>
-      <Button type="link">Primary Button</Button>
     </main>
   );
 }
